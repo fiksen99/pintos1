c2ae1b00d21e73213d49d46b551823d3f773e376
Sahil Jain <sdj09@ic.ac.uk>
2012-02-09 19:57:58 +0000

    Sorted waiters

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -68,7 +68,7 @@ sema_down (struct semaphore *sema)
   old_level = intr_disable ();
   while (sema->value == 0) 
     {
-      list_push_back (&sema->waiters, &thread_current ()->elem);
+      list_insert_ordered (&sema->waiters, &thread_current ()->elem, compare_priority_less, NULL);
       thread_block ();
     }
   sema->value--;
@@ -113,7 +113,7 @@ sema_up (struct semaphore *sema)
   ASSERT (sema != NULL);
 
   old_level = intr_disable ();
-  list_sort (&sema->waiters, compare_priority_less, NULL);
+  //list_sort (&sema->waiters, compare_priority_less, NULL);
   sema->value++;
   if (!list_empty (&sema->waiters)) 
     thread_unblock (list_entry (list_pop_back (&sema->waiters), struct thread, elem));
